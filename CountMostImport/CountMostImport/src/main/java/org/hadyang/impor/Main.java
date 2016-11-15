271c800198c978cabf70958f0bd352b615117019
HaoyangShi <shihaoyang129@gmail.com>
2016-11-15 12:34:07 +0800

    修改结果显示

--- CountMostImport/CountMostImport/src/main/java/org/hadyang/impor/Main.java
+++ CountMostImport/CountMostImport/src/main/java/org/hadyang/impor/Main.java
@@ -1,6 +1,8 @@
 package org.hadyang.impor;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,16 +19,22 @@ public class Main {
             return;
         }
 
+        if (!file.isDirectory()){
+            System.out.println("该路径不是文件夹路径！");
+            return;
+        }
+
         CountMostImport countMostImport = new CountMostImport();
         List<CountResult> data = countMostImport.count(file);
 
         TopK<CountResult> topK = new TopK<>();
-        List<CountResult> result = topK.getTopK(data, 10);
-
-        System.out.println("该目录下被 Import 次数最多的类，前十分别是：");
+        int k = data.size() > 10 ? 10 : data.size();
+        List<CountResult> result = topK.getTopK(data, k);
+        Collections.sort(result, (o1, o2) -> o2.count - o1.count);
+        System.out.println(String.format("该目录下被 Import 次数最多的类，前 %d 分别是：", k));
 
         for (CountResult countResult : result) {
-            System.out.println(String.format("%s:被 Import %d 次", countResult.name, countResult.count));
+            System.out.println(String.format("%s :被 Import %d 次", countResult.name, countResult.count));
         }
     }
 }
