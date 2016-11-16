7aa5c70e879da325e62df36fbc089700c10d1202
jackwang <wdmzjjm@163.com>
2016-11-16 11:21:22 +0800

    simple animation

--- work1/index.js
+++ work1/index.js
@@ -1,6 +1,9 @@
 /**
  * Created by jackwang on 2016/11/14.
  */
+function getId(name){
+    return document.getElementById(name);
+}
 function listen(ele, type, handler){
     if(document.addEventListener){
         ele.addEventListener(type, handler, false);
@@ -10,7 +13,23 @@ function listen(ele, type, handler){
         ele['on' + type] = handler;
     }
 }
-var returnButton = document.getElementById('return_top');
-listen(returnButton, 'click', function(){
-    document.documentElement.scrollTop = document.body.scrollTop = 0;
+listen(getId('return_top'), 'click', function(){
+    var scrollTopStep, animation;
+    if(document.body.scrollTop){
+        scrollTopStep = document.body.scrollTop / 100;
+        animation = setInterval(function(){
+            if(document.body.scrollTop <= 0){
+                clearInterval(animation);
+            }
+            document.body.scrollTop -= scrollTopStep;
+        }, 0);
+    }else{
+        scrollTopStep = document.documentElement.scrollTop / 100;
+        animation = setInterval(function(){
+            if(document.documentElement.scrollTop <= 0){
+                clearInterval(animation);
+            }
+            document.documentElement.scrollTop -= scrollTopStep;
+        }, 0);
+    }
 });
