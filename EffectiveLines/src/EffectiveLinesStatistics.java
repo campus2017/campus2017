68f56917790c06bbfa3e48ec645487c05bbb9a92
leeyangbupt <leeyang_bupt@163.com>
2016-12-22 23:07:49 +0800

    start coding

--- EffectiveLines/src/EffectiveLinesStatistics.java
+++ EffectiveLines/src/EffectiveLinesStatistics.java
@@ -1,12 +1,9 @@
 
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 
 /**
- * 统计java文件的有效行
+ * 统计一个java文件的有效行
  *  1.有效行不包括空行
  *  2.不考虑代码间有多行注释的情况
  */
@@ -22,8 +19,20 @@ public class EffectiveLinesStatistics {
      *
      * 统计Java源文件数和有效行数
      */
-    public static void statistics(File file) {
-        if (file.isFile())
-    }
 
+    public static void statistics(File inFile)
+            throws IOException {
+        for (File file : inFile.listFiles(new JavaFileFilter()) ) {
+            if (file.isFile()) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(file),"utf-8"));
+                String tmp;
+                while ((tmp = br.readLine()) != null) {
+
+                }
+            } else if (file.isDirectory()) {
+                statistics(file);
+            }
+        }
+    }
 }
