64fb6abaf194233733e3b2d4ba8fa47350063e1f
leeyangbupt <leeyang_bupt@163.com>
2016-12-29 19:01:25 +0800

    Basic function have done!

--- CountMostImport/src/Main.java
+++ CountMostImport/src/Main.java
@@ -1,36 +1,72 @@
 
 import leeyang.practice.qunar.MapClassFromFile;
+import leeyang.practice.qunar.TopKItem;
 
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.File;
+import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+
 
 /**
  * 统计指定项目目录下import最多的类，前k个
  */
 
 public class Main {
+
     public static void main(String[] args) {
-        String path = getInput();
+        String path = getFilePath();
+        int k = getK();
         File inFile = new File(path);
         try {
             MapClassFromFile.readFile(inFile);
             HashMap<String, Integer> mapClass = (HashMap<String, Integer>) MapClassFromFile.mapClass;
-            Integer[] temp = mapClass.values().toArray(new Integer[mapClass.size()]);
-            System.out.println(mapClass);
-            for (int v : temp)
-                System.out.println(v);
+            Integer[] classCount = mapClass.values().toArray(new Integer[mapClass.size()]);
+            TopKItem topK = new TopKItem(classCount,k);
+            List<Integer> topData = topK.getResult();
+            Map<String, Integer> topClassName = getTopClassName(mapClass, topData);
+            System.out.println(inFile + " 目录下import最多的前" + k + "个类为:");
+            printResult(topClassName);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String getInput() {
+    private static Map<String, Integer> getTopClassName(Map<String, Integer> mapData, List<Integer> topData) {
+        Map<String, Integer> res = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : mapData.entrySet()) {
+            if (topData.contains(entry.getValue())) {
+                res.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return res;
+    }
+
+    private static void printResult(Map<String, Integer> mapResult) {
+
+        for (Map.Entry<String, Integer> entry : mapResult.entrySet()) {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+    }
+
+    private static String getFilePath() {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入文件:");
-        String path = sc.nextLine();
-        return path;
+        return sc.nextLine();
+    }
+
+    private static int getK() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入需要提取的前N个数:");
+        int k = 1;
+        try {
+             k = Integer.parseInt(sc.nextLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return k;
     }
 }
--- CountMostImport/src/leeyang/practice/qunar/TopKItem.java
+++ CountMostImport/src/leeyang/practice/qunar/TopKItem.java
@@ -1,5 +1,7 @@
 package leeyang.practice.qunar;
 
+import java.util.List;
+import java.util.Arrays;
 
 /**
  * 提取topK数量多的项
@@ -8,5 +10,31 @@ package leeyang.practice.qunar;
 
 
 public class TopKItem {
+    private Integer[] data;
+    private int k;
+    public TopKItem(Integer[] data, int k) {
+        this.data = data;
+        this.k = k;
+    }
 
+    public List<Integer> getResult() {
+        if (k > data.length) {
+            return Arrays.asList(data);
+        }
+
+        Integer[] heapData = new Integer[k];
+        for (int i = 0; i < k; i++) {
+            heapData[i] = data[i];
+        }
+        MinHeap heap = new MinHeap(heapData);
+
+        for (int i = k; i < data.length; i++) {
+            int rootData = heap.getRoot();
+            if (data[i] > rootData) {
+                heap.setRoot(data[i]);
+            }
+        }
+       // List<Integer> list = new ArrayList<Integer>();
+        return Arrays.asList(heapData);
+    }
 }
