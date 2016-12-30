c389f6ec5be93b9ef116f466b8af96768658561e
chuangfeng.wang <chuangfengwang@163.com>
2016-12-30 18:31:53 +0800

    第三个作业:统计被 import 最多的10个类

---
+++ CountMostImport/src/main/java/com/qunar/wireless/wcf/Main.java
@@ -0,0 +1,25 @@
+package com.qunar.wireless.wcf;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wcf on 2016-12-30.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        if(args.length>1){
+            System.out.println("参数太多了!");
+        }else if(args.length<1){
+            System.out.println("请提供java源代码的目录!");
+        }else{
+            List<Map.Entry<String,Integer>> r = CountMostImport.topKImportClass(args[0],10);
+            System.out.printf("======被import最多的是%d个类是=====\n",10);
+            for(Map.Entry<String, Integer> ic: r){
+                System.out.println(ic.getKey()+":"+ic.getValue());
+            }
+        }
+
+    }
+}
