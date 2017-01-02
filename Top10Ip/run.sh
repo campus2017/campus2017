8e9d725c93643259aa047148a96010f7a7c060df
leeyangbupt <leeyang_bupt@163.com>
2017-01-02 19:55:09 +0800

    done Top10Ip

--- Top10Ip/run.sh
+++ Top10Ip/run.sh
@@ -2,7 +2,6 @@
 
 
 #递归读取文件夹下的所有文件
-
 function read_dir() {
     for file in `ls $1`
     do 
@@ -17,4 +16,4 @@ function read_dir() {
 }
 
 #调用读取目录下文件的函数来统计日志文件中访问次数最多的10的IP
-read_dir $1 | awk '{print $1}' | sort |uniq -c |sort -rn -k1,1 |head -n 10  
+read_dir $1 | awk '{print $1}'| sort |uniq -c |sort -rn -k1,1 |head -n 10  
