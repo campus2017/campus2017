17d434f563be60f006beef2604f427cfd2c92361
leeyangbupt <leeyang_bupt@163.com>
2017-01-02 19:52:56 +0800

    done Top10Ip

--- Top10Ip/README.md
+++ Top10Ip/README.md
@@ -1,2 +1,5 @@
 ##任务##
 * 统计列出所有日志文件(tomcat的access log) 中，访问次数最多的10个IP,以及对应的次数。
+
+###调用方法###
+* sh run.sh [dirpath]
---
+++ Top10Ip/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#递归读取文件夹下的所有文件
+
+function read_dir() {
+    for file in `ls $1`
+    do 
+        if [ -d $1"/"$file ]
+        then
+            read_dir $1"/"$file
+        else
+            cat $1"/"$file
+
+        fi
+    done 
+}
+
+#调用读取目录下文件的函数来统计日志文件中访问次数最多的10的IP
+read_dir $1 | awk '{print $1}' | sort |uniq -c |sort -rn -k1,1 |head -n 10  
