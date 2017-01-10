8a3ef861c2bc21a2f0c59fe4666780d682a9f5eb
Nan.Zhang <ZN_HIT@163.com>
2017-01-10 16:16:26 +0800

    first commit

---
+++ QunarHomework/EffectiveLines/TestFile.java
@@ -0,0 +1,40 @@
+/**
+ * This is a test file.
+ * @version 1  2016-12-23
+ * @author Abby
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.PrincipalComponents;
+
+public class PCA {
+	protected static void pca(Instances data) throws Exception {
+		PrincipalComponents pca = new PrincipalComponents();
+		pca.setInputFormat(data);
+		pca.setMaximumAttributes(5);
+//test file
+		Instances newData = Filter.useFilter(data, pca);
+		System.out.println("newdata" + newData);
+
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(newData);
+		saver.setFile(new File("PCAresult.arff"));
+		saver.writeBatch();
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new FileReader("d.arff"));
+		Instances data = new Instances(reader);
+		reader.close();
+		if (data.classIndex() == -1) {
+			data.setClassIndex(data.numAttributes() - 1);
+		}
+		pca(data);
+	}
+}
