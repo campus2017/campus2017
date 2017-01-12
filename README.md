45c77264ae3a96e58c1d7a228c672cf772cce0fe
zhangyulei <1241398344@qq.com>
2017-01-12 19:07:06 +0800

    fixed some IE8 show bug

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # campus2017
 2017应届生入职前自学作业提交
+
+### 2017-1-12改动
++ 修复task1中img链接在IE9下border显示的问题
++ 利用filter实现task2 IE8下透明的效果
++ 修复task3中inputIE8下偏移问题
--- task2/1/js/index.js
+++ task2/1/js/index.js
@@ -1,8 +1,11 @@
 function changeLayerState(flag) {
+    var layer = document.getElementById("layer");
     var layerId = document.getElementById("layer-bg");
     if (flag === 'close') {
         layerId.style.display = "none";
+        layer.style.display = "none";
     } else {
         layerId.style.display = "block";
+        layer.style.display = "block";
     }
 }
