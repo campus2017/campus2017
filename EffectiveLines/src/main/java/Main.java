d49b11425d5dd7725c49c59abc251b97f8de6466
liuhuaxuan <1129655300@qq.com>
2017-04-21 16:06:55 +0800

    简单调整了顺序

--- EffectiveLines/src/main/java/Main.java
+++ EffectiveLines/src/main/java/Main.java
@@ -5,25 +5,10 @@ import java.util.Scanner;
 
 import com.google.common.base.Strings;
 /**
- * Created by asus on 2017/1/16.
+ * Created by asus on 2017/4/21.
  */
 public class Main {
-    public static int count(String filepath) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader(filepath));
-        String line = null;
-        int count =0;
-        while ((line=reader.readLine())!=null){
-            line = line.trim();
-            if (Strings.isNullOrEmpty(line)){
-                continue;
-            }else if((line.startsWith("/*")&&line.endsWith("*/"))){
-                continue;
-            }
-            count++;
-        }
-        reader.close();
-        return count;
-    }
+
     public static void main(String []args){
         System.out.println("请输入文件路径：");
 
@@ -41,6 +26,22 @@ public class Main {
                 e.printStackTrace();
             }
         }
+    }
+    public static int count(String filepath) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        String line = null;
+        int count =0;
+        while ((line=reader.readLine())!=null){
+            line = line.trim();
+            if (Strings.isNullOrEmpty(line)){
+                continue;
+            }else if((line.startsWith("/*")&&line.endsWith("*/"))){
+                continue;
+            }
+            count++;
+        }
+        reader.close();
+        return count;
     }
 
 }
