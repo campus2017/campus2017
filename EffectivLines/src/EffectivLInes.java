0dd77afa0a736d3e84e4b2908e5d733e1d509f43
pengweiye <384438817@qq.com>
2017-06-09 12:25:56 +0800

    EffectivLInes completed

--- EffectivLines/src/EffectivLInes.java
+++ EffectivLines/src/EffectivLInes.java
@@ -1,6 +1,38 @@
+import java.io.*;
+
+import static java.lang.System.*;
+
 /**
  * Created by root on 6/7/17.
  */
 public class EffectivLInes
 {
+    static public void main(String args[])
+    {
+        if(args.length != 1)
+        {
+            out.print("require path\r\n");
+            return;
+        }
+        String fileName = args[0];
+        try
+        {
+            int count = 0;
+            String tmp = null;
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader((new FileInputStream(fileName))));
+            while((tmp = reader.readLine()) != null)
+            {
+                if(!tmp.equals(""))
+                    count++;
+            }
+            out.print(count);
+            out.print("\r\n");
+        }
+        catch (Exception e)
+        {
+            out.print("file not found");
+        }
+
+    }
 }
