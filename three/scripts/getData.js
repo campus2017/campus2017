10792598251a48370ef2f9972b7a8a8338a472fd
Chunqiang Li <Chunqiang Li>
2017-06-13 23:16:23 +0800

    作业三，页面实现了一部分

--- three/scripts/getData.js
+++ three/scripts/getData.js
@@ -1,10 +1,3 @@
-/* 
-* @Author: lichunqiang
-* @Date:   2017-06-12 20:27:19
-* @Last Modified by:   anchen
-* @Last Modified time: 2017-06-12 23:30:09
-*/
-
 $(document).ready(function(){
 
     const getDataPromise = new function(){
