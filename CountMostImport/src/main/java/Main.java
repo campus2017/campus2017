a3b025d164bbd08d98466cf60adce4cca2cf1da3
stamwoo <swstamwoo@gmail.com>
2017-06-24 23:19:00 +0800

    fix wrong words

--- CountMostImport/src/main/java/Main.java
+++ CountMostImport/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         fileName = input.nextLine();
         List<Map.Entry<String,Integer>> r = CountMostImport.topKImportClass(fileName,10);
-        System.out.printf("======被import最多的是%d个类是=====\n",10);
+        System.out.printf("======被import最多的%d个类是=====\n",10);
         for(Map.Entry<String, Integer> ic: r){
             System.out.println(ic.getKey()+":"+ic.getValue());
         }
