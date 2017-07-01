05619d77ed8e7ee0a4ec708b55ace59734ddeed9
wwy941122 <wwy941122@qq.com>
2017-07-01 23:22:04 +0800

    final

--- QunarTest/CountMostImport/src/Main.java
+++ QunarTest/CountMostImport/src/Main.java
@@ -2,80 +2,101 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Created by zhuyin on 16-11-23.
+ * Created by WangWeiyi on 2017/1/7 0007.
  */
 public class Main {
 
-    public static void main(String args[]){
-        String path = "ExchangeRate/src/main";
-        List<File> files = getFile(path,new ArrayList<File>());
-        Map<String,Integer> ret = countImportNum(files);
-        for(Map.Entry<String,Integer> entry:ret.entrySet()){
-            System.out.println("class: "+entry.getKey()+" nums；"+entry.getValue());
-        }
-    }
+    public static void main(String[] args){
+        String path = "";
+        System.out.println("请输入需要统计的目录");
+        Scanner in = new Scanner(System.in);
+        path = in.nextLine();
+        //path = "C:\\\\Program Files\\\\Java\\\\jdk1.8.0_45\\\\src";
 
-    private static Map<String,Integer> countImportNum(List<File> files) {
-        Map<String,Integer> map = new HashMap();
-
-        Map<String,Integer> ret = new HashMap<>();
-        
-        if(files.size()==0)
-            return null;
-        for(File f:files) {
-            readFile(f, map);
-        }
-
-        List<Integer> res = FindKthLargest.findKthNum(map.values().toArray(new Integer[map.values().size()]),10);
-
-        for(Map.Entry<String,Integer> entry : map.entrySet()){
-            int v = entry.getValue();
-            if(res.contains(v))
-                ret.put(entry.getKey(),entry.getValue());
-        }
-        return ret;
-    }
-
-    private static void readFile(File file, Map<String, Integer> map) {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String line = "";
-            while((line=reader.readLine())!=null){
-                if(line.startsWith("import")){
-                    line = line.replaceAll("\\s","");
-                    line = line.substring(6);
-                    if(map.get(line)==null)
-                        map.put(line,1);
-                    else
-                        map.put(line,map.get(line)+1);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
+        List<File> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        list = getFiles(path, list);
+        for(File f : list){
             try {
-                reader.close();
+                map = getImport(f, map);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        map = sortImport(map);
+
+        System.out.println("统计结果如下：");
+        Iterator it = map.entrySet().iterator();
+        int count=1;
+        while(it.hasNext()){
+            Map.Entry entry = (Map.Entry) it.next();
+            System.out.println(count+":"+entry.getKey() + ":" + entry.getValue());
+            count++;
+            if(count >  10)
+                break;
+        }
+    }
+    public static Map<String, Integer> sortImport(Map<String, Integer> map){
+        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+        Map<String, Integer> result = new LinkedHashMap<String, Integer>();
+        for(Map.Entry<String, Integer> entry : list){
+            result.put(entry.getKey(),entry.getValue());
+        }
+        return result;
     }
 
-    private static List<File> getFile(String path,List<File> fileList) {
-        File file = new File(path);
-        if(file.exists()){
-            File[] files = file.listFiles();
-            if(files.length>0){
-                for(File f:files){
-                    if(f.isDirectory()){
-                        getFile(f.getAbsolutePath(),fileList);
-                    }else{
-                        fileList.add(f);
+    //读取文件中的引用信息
+    public static Map<String, Integer> getImport(File file, Map<String, Integer> map) throws IOException {
+        BufferedReader br = null;
+        InputStreamReader sr = null;
+        try {
+            sr = new InputStreamReader(new FileInputStream(file));
+            br = new BufferedReader(sr);
+            String str = "";
+            while((str = br.readLine())!= null){
+                if(str.startsWith("import")){
+                    str = str.replaceAll("\\s", "").substring(6);
+                    if(!map.containsKey(str) ){
+                        map.put(str, 1);
+                    } else {
+                        map.put(str, map.get(str)+1);
                     }
                 }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+            sr.close();
         }
-        return fileList;
+        return map;
     }
+
+    //深度优先遍历，获取list
+    public static List<File> getFiles(String path, List<File> list){
+        File file = new File(path);
+        if(file.exists()){
+            File[] files = file.listFiles();
+            if(files.length > 0){
+                for(File f : files){
+                    if(f.isDirectory()){
+                        getFiles(f.getAbsolutePath(),list);
+                    } else {
+                        list.add(f);
+                    }
+                }
+            }
+
+        }
+        return list;
+    }
+
 }
--- QunarTest/EffectiveLines/src/Main.java
+++ QunarTest/EffectiveLines/src/Main.java
@@ -1,59 +1,55 @@
 import java.io.*;
+import java.util.Scanner;
 
 /**
- * Created by zhuyin on 16-11-15.
+ * Created by WangWeiyi on 2017/1/7 0007.
  */
 public class Main {
-    public static void main(String args[]){
-        String path = "EffectiveLines/resource/test.txt";
-        File file = new File(path);
-        boolean vaild = checkPath(file);
-        if(!vaild)
-            System.out.println("file is not exit");
-        else
-            //read
-            System.out.println(countLines(file));
-    }
-    //check file
-    private static boolean checkPath(File file){
-        if(file.exists())
-            return true;
-        else
-            return false;
-    }
+    public static void main(String[] args){
+        String filePath = "";
+        System.out.println("请输入要统计的文件路径");
+        Scanner in = new Scanner(System.in);
+        filePath = in.nextLine();
+        //filePath = "E:/QunarTest/EffectiveLines/test/Calculate.java";
+        File file = new File(filePath);
+        if(file.exists()){
+            try {
 
-    //count lines
-    private static int countLines(File file){
-        BufferedReader reader = null;
-        int lineNum = 0;
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            try {
-                while((line=reader.readLine())!=null){
-                    if(lineIsValid(line))
-                        lineNum++;
-                }
+                System.out.println("该文件有效行数为：" + count(file));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } else {
+            System.out.println("can't find the file!");
         }
-        return lineNum;
     }
+    //有效行计数
+    private static int count(File file) throws IOException {
+        int lineCount = 0;
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(file));
+        String line = null;
+        try {
+            while((line = br.readLine()) != null){
+                if(isValid(line)){
+                    lineCount++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
+        return  lineCount;
 
-    private static boolean lineIsValid(String line){
+    }
+    //判断是否是有效行
+    private static boolean isValid(String line){
         line = line.trim();
-        if("".equals(line)||line.startsWith("//")||(line.startsWith("/*")&&line.endsWith("/*")))
+        if("".equals(line)||line.startsWith("//")||(line.startsWith("/*")&&line.endsWith("*/"))){
             return false;
-        else
+        } else {
             return true;
+        }
     }
 }
