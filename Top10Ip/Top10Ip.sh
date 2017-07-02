3b2abd26bc4f67f20690bfa9d45b5a09d45cbf69
qingsai.zhang <1026607552@qq.com>
2017-07-02 18:08:03 +0800

    提交Linux作业

--- Top10Ip/Top10Ip.sh
+++ Top10Ip/Top10Ip.sh
@@ -1,4 +1,1 @@
-# calculate top 10 ip access
-#!/bin/bash
-
-awk '{print $1}' /opt/apache-tomcat-8.5.14/logs/localhost_access_log.* |sort| uniq -c | sort -rn | head -n 10
+awk '{print $1}' /opt/apache-tomcat-8.5.14/logs/localhost_access_log.* |sort| sort -r -n | head -n 10
