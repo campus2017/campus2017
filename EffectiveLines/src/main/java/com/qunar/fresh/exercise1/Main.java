ec2e97a7040510266034853ffd946bacddbcd99c
wupei124 <865508309@qq.com>
2017-07-02 12:49:42 +0800

    clean

--- EffectiveLines/src/main/java/com/qunar/fresh/exercise1/Main.java
+++ EffectiveLines/src/main/java/com/qunar/fresh/exercise1/Main.java
@@ -1,34 +1,43 @@
 package com.qunar.fresh.exercise1;
 
-import java.io.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by wupei on 2017/6/30.
  */
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        // reader
+        String fileName = "src\\main\\java\\com\\qunar\\fresh\\exercise1\\Main.java";
         BufferedReader br = null;
         try {
-            FileReader fr = new FileReader("src\\main\\java\\com\\qunar\\fresh\\exercise1\\Main.java");
+            FileReader fr = new FileReader(fileName);
             br = new BufferedReader(fr);
             String str = br.readLine();
             int line = 0;
             while (str != null) {
-                if (str.trim().length() != 0) {
+                if (str.trim().length() != 0 && !str.trim().startsWith("//")) {
                     line++;
                 }
                 str = br.readLine();
             }
             System.out.println(line);
         } catch (java.io.IOException e) {
-            e.printStackTrace();
+            LOGGER.error("读文件异常, file:{}", fileName, e);
         } finally {
             try {
                 if (br != null) {
                     br.close();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.error("关闭文件异常, file:{}", fileName, e);
             }
         }
     }
