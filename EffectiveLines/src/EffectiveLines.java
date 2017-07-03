b22f676ad551c0198400f94f99fec6c11b727149
hello-cyz <365911132@qq.com>
2017-07-03 23:37:16 +0800

    Update EffectiveLines.java

--- EffectiveLines/src/EffectiveLines.java
+++ EffectiveLines/src/EffectiveLines.java
@@ -1,91 +1,59 @@
-import java.io.*;
+package qunar.yaozhu;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
- * Created by isc on 2016/11/20.
+ * 统计一个Java文件的有效行数。（作业命名：EffectiveLines）
+ *1、有效不包括空行
+ *2、不考虑代码见有多行注释的情况
  */
 public class EffectiveLines {
-
-    public static void main(String[]args){
-        String filePath = "G:\\IDEA_Workspace_2016_12_16\\EffectiveLines\\src\\Test2.java";
-        System.out.println(calculateLinesAccordingPath(filePath));
-    }
-
-    private static String calculateLinesAccordingPath(String path) {
-        File file = new File(path);
-        return countCodeLines(file);
-    }
-
-    /**
-     *
-     * @param file
-     * @return 行数
-     */
-    private static String countCodeLines(File file){
-        int lines=0;
-        //判断是否为.java文件
-        if(isJavaFile(file)){
-            FileReader filereader = null;
-            BufferedReader bufferReader=null;
-            try{
-                filereader = new FileReader(file);
-                bufferReader = new BufferedReader(filereader);
-                String lineContent;
-                while(bufferReader.ready()){
-                    //去掉开头可能存在的空格
-                    lineContent = bufferReader.readLine().trim();
-                   if(isBlankLine(lineContent))
-                        continue;
-                    if(isAnnotation(lineContent))
-                        continue;
-                    lines++;
-                }
-            }catch (Exception e){
-                return e.getMessage();
-            }finally {
-                closeReader(filereader,bufferReader);
-            }
-        }
-        return String.valueOf(lines) ;
-    }
-
-    private static boolean isJavaFile(File file){
-        if(file.getName().matches(".*\\.java$")) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isBlankLine(String lineContent){
-        if(lineContent.matches("^[\\s && [^\\n]]*$"))
-            return true;
-        return false;
-    }
-
-    /*   注释的情况:
-    * 1. /* 2. // 3. * 4.  */
-    private static boolean isAnnotation(String lineContent){
-
-        if((lineContent.matches("^/\\*.*")))  //   表示/*  \\表示转义  \\* —> *
-            return true;
-        if (lineContent.matches("^//.*"))    //  表示//
-            return true;
-        if(lineContent.matches("^\\*.*"))    // 表示 *
-            return true;
-        if(lineContent.matches(".*\\*/"))  //表示  */
-            return true;
-        return  false;
-    }
-
-    private static void closeReader(FileReader fileReader,BufferedReader bufferedReader){
-        if (bufferedReader != null) {
-            try {
-                bufferedReader.close();
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	
+	public static void main(String[] args) {
+		int lineNum = 0;
+		System.out.print("Pleast input the file path:");
+		Scanner scanner = new Scanner(System.in);
+		String filePath = scanner.nextLine();
+		scanner.close();
+		File file = new File(filePath);
+		if(!file.exists())
+			System.out.println("File is not exit!");
+		else if (!file.getName().endsWith(".java"))
+			System.out.println("The file is not java file!");
+		else{
+			lineNum = new EffectiveLines().linesOfFile(filePath);
+			System.out.println("The effective lines of file" + file.getName() + "is:" + lineNum);
+		}	
+	}
+	public int linesOfFile(String filePath){
+		int lineNum = 0;
+		String line = "";
+		try {
+			BufferedReader breader = new BufferedReader(new FileReader(filePath));
+			while((line = breader.readLine()) != null){
+				if (line.startsWith("/*")&&line.endsWith("*/"))
+					continue;
+				else if(line.trim().equals(""))
+					continue;
+				else if(line.startsWith("/*")&&!line.endsWith("*/")){
+					while(!line.endsWith("*/")){
+						line = breader.readLine();
+					}
+				} else{
+					lineNum++;
+				}		
+			}	
+			breader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lineNum;
+	}
 }
