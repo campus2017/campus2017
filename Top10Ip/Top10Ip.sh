dedb5a8b7b72271891d46d01709836d6d810ebe9
hello-cyz <365911132@qq.com>
2017-07-03 23:35:08 +0800

    Update Top10Ip.sh

--- Top10Ip/Top10Ip.sh
+++ Top10Ip/Top10Ip.sh
@@ -1,4 +1,9 @@
-# calculate top 10 ip access
-#!/bin/bash
+{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf830
+{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red36\green41\blue46;}
+{\*\expandedcolortbl;;\csgenericrgb\c14118\c16078\c18039;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\li360\sb156\pardirnatural\partightenfactor0
 
-awk '{print $1}' /opt/apache-tomcat-8.5.14/logs/localhost_access_log.* |sort| uniq -c | sort -rn | head -n 10
+\f0\fs28 \cf2 awk '\{print $1\}' access log |uniq -c|sort -nr|head -n 10}
