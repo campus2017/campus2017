3f11b6b9ddd32c109cfeb7d4a973c2478d1cd283
hello-cyz <365911132@qq.com>
2017-07-03 23:35:40 +0800

    Update linux2.sh

--- linux2/linux2.sh
+++ linux2/linux2.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-#copy file from local to dest
-#should input the filename
+{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf830
+{\fonttbl\f0\froman\fcharset0 Times-Roman;\f1\fnil\fcharset134 STSongti-SC-Regular;}
+{\colortbl;\red255\green255\blue255;\red51\green51\blue51;}
+{\*\expandedcolortbl;;\csgenericrgb\c20000\c20000\c20000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sb156\pardirnatural\partightenfactor0
 
-scp dir1/$1 root@l-test.dev.cn1:/tmp
+\f0\fs28 \cf2 scp  -r  /dir1/
+\f1 \'ce\'c4\'bc\'fe
+\f0  root@ l-test.dev.cn1: /temp/}
