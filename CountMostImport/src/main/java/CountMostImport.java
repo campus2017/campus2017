1fc918fd42a9341a51597d7b4a1351d4549e5eaf
hello-cyz <365911132@qq.com>
2017-07-03 23:34:27 +0800

    Update CountMostImport.java

--- CountMostImport/src/main/java/CountMostImport.java
+++ CountMostImport/src/main/java/CountMostImport.java
@@ -1,103 +1,85 @@
+package qunar.yaozhu;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * Created by isc on 2016/12/19.
+ * 根据指定项目目录下（可以认为是java源文件目录）中，统计被import最多的类，前十个是什么
+ * 作业命名(CountMostImport)
  */
 public class CountMostImport {
+    private Map<String, Integer> map = new HashMap<String, Integer>();
 
-    private String dir;
-    private HashMap<String,Integer> importRecords;
-
-    public CountMostImport(String dir){
-        this.dir=dir;
-        importRecords = new HashMap<String, Integer>();
-        this.staticsAllFiles(new File(dir));
+    public static void main(String[] args) throws Exception {
+        CountMostImport countMostImport = new CountMostImport();
+        System.out.print("Pleast input the file path:");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        scanner.close();
+        countMostImport.countImport(path);
+        
     }
 
-
-    /**
-     * 利用小根堆求取Top K数据
-     * @param k
-     */
-    public void getTopKParams(int k){
-        List<MostImportClass> list = TopK.getTopK(changeHashMapToList(),k);
-        for(int i=0;i<list.size();i++) {
-            System.out.println(list.get(i).importName+":"+list.get(i).value);
-        }
+    public void countImport(String dirPath) throws Exception {
+        File file = new File(dirPath);
+        depthSearch(file);
+        sortMapByCollection();
     }
 
-    /**
-     * 遍历文件和文件夹
-     * @param file
-     */
-    private void staticsAllFiles(File file){
-        if(!file.isDirectory()){
-            processFile(file);
-        }else{
-            File[] files = file.listFiles();
-            for(File f:files){
-                staticsAllFiles(f);
-            }
-        }
-    }
-
-    /**
-     * 加载文件
-     * @param file
-     */
-    private void processFile(File file){
-        BufferedReader reader=null;
-        try {
-            reader = new BufferedReader(new FileReader(file));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return;
-        }
-        String line = null;
-        try {
-            while((line=reader.readLine())!=null){
-                line = line.trim();
-                if(line.startsWith("public")||line.startsWith("class")){
-                    break;
-                } if(line.startsWith("import")) {
-                    putParamsToHashMap(line);
+    public String depthSearch(File f) throws Exception {
+        if (f.isDirectory()) {
+            File[] files = f.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    depthSearch(file);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 把加载数据放入HashMap中
-     * @param line
-     */
-    private void putParamsToHashMap(String line) {
-        String className = line.substring(6, line.length() - 1).trim();
-        Integer value = importRecords.get(className);
-        if (value == null) {
-            importRecords.put(className, 1);
         } else {
-            importRecords.put(className, 1 + value);
+            findNumberofImport(f);
         }
+        return null;
     }
 
-    /**
-     * 把HashMap中的数据转换成list<MostImortClass>
-     * @return
-     */
-    private List<MostImportClass> changeHashMapToList(){
-        List<MostImportClass> list = new ArrayList<MostImportClass>();
-        for(Map.Entry item: this.importRecords.entrySet()){
-            String key = (String) item.getKey();
-            int value = (Integer)item.getValue();
-            list.add(new MostImportClass(key,value));
+    private void findNumberofImport(File f) throws Exception {
+        
+        int count = 0;
+        InputStream in = new FileInputStream(f);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            line = line.trim();
+            if (line.startsWith("import")) {
+                //只保留引用的包名
+                line = line.substring(6);
+                if (!map.containsKey(line)) {
+                    map.put(line, 1);
+                } else {
+                    int value = map.get(line);
+                    map.put(line, value + 1);
+                }
+            }          
         }
-        return list;
+        reader.close();
+    }
+
+    //按照value数值排序Map，并且输出最多引用和前十引用。
+    public void sortMapByCollection(){
+        Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        };
+        List<Map.Entry<String, Integer>> listOfTheMap = new ArrayList<>(map.entrySet());
+        listOfTheMap.sort(valueComparator);
+
+        System.out.println("The most import is : " + listOfTheMap.get(0).getKey() + " : " + listOfTheMap.get(0).getValue());
+
+        System.out.println("Top 10 of the import is : ");
+        for(int i = 0; i < 10; i++){
+            System.out.println(listOfTheMap.get(i).getKey() + " : " + listOfTheMap.get(i).getValue());
+        }
+
     }
 
 }
