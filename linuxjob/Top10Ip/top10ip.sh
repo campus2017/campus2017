dccfad9401b8bbc3cc1ea23accf3d04c6e5ec7ff
shengdaguy <li1113183271@163.com>
2017-07-04 08:24:08 +0800

    remove the other

--- linuxjob/Top10Ip/top10ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-res=`awk '{ print $1 }' ./tomcat_access_log.txt | sort |  uniq -c | sort -nr | head -n 10`
-
-echo $res
-
--- linuxjob/linux2/linux2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#use scp command
-scp ./dir1/testfile   l-qsrc1.sec.beta.cn0:/tmp/
-
