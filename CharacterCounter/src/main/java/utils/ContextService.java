b1fa25960cb21d55b6c7ef71267b1f7c378805d6
houpangtun <1184716452@qq.com>
2017-07-06 17:13:54 +0800

    ceshi

--- CharacterCounter/src/main/java/utils/ContextService.java
+++ CharacterCounter/src/main/java/utils/ContextService.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
  * Created by luotuomianyang on 2017/1/28.
  */
 public interface ContextService {
+
     Statistics statContext(Context context);
     Statistics statFile(MultipartFile file);
 }
